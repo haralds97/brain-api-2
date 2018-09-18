0cb68934bd90b5192f9732b474639e3a78652514
haraldsDev <Haralds.Dev@gmail.com>
2018-09-18 17:54:42 +0300

    small changes to server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 	users: [
@@ -23,10 +25,10 @@ const database = {
 			joined: new Date()
 		}
 	]
-}
+};
 
 app.get('/', (req, res) => {
-	res.json('it is working');
+	res.json(database.users);
 })
 
 app.post('/signin', (req, res) => {
