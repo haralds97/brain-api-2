c23d30ee387a9f71795884506223e6fdb0741b31
haraldsDev <Haralds.Dev@gmail.com>
2018-10-04 21:03:52 +0300

    added bcrypt package. added trx object to register endpoint, to have login-users tables related

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
 
 const db = knex({
   client: 'pg',
@@ -55,17 +56,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { name, email, password } = req.body;
-	db('users')
-		.returning('*')
-		.insert({
-			email: email,
-			name: name,
-			joined: new Date()
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
 		})
-	  .then(user => {
-		res.json(user[0]);
-	  })
-	  .catch(err => res.status(400).json('unable to register'));
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({
+				email: loginEmail[0],
+				name: name,
+				joined: new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			 })
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+    .catch(err => res.status(400).json('unable to register'));
 })
 
 app.get('/profile/:id', (req, res) => {
