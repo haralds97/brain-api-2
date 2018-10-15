7135ede3f57de2667e9aed68902cb8d00734528b
haraldsDev <Haralds.Dev@gmail.com>
2018-10-15 14:21:03 +0300

    added bcrypt hash to 'register' endpoint + transaction() method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
 
 const db = knex({
   client: 'pg',
@@ -53,17 +54,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { name, email, password } = req.body;
-	db('users')
-		.returning('*')
-		.insert({
-			name: name,
-			email: email,
-			joined: new Date()
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
 		})
-		.then(user => {
-			res.json(user[0]);
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({
+				name: name,
+				email: loginEmail[0],
+				joined: new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
 		})
-		.catch(err => res.status(400).json('smth went wrong'))
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('smth went wrong'))
 })
 
 app.get('/profile/:id', (req, res) => {
