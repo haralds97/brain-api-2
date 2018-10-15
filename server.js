d78dcfb894f23e73313b25b5a202aab47acb5338
haraldsDev <Haralds.Dev@gmail.com>
2018-10-15 22:11:23 +0300

    finished register and signin endpoints :)

--- server.js
+++ server.js
@@ -44,12 +44,22 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-	res.json('success ... success')
-	} else {
-		res.status(404).json('error loggin in...');
-	}
+	db.select('email', 'hash').from('login')
+		.where('email','=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db.select('*').from('users')
+				.where('email', '=', req.body.email)
+				then(user => {
+					res.json(user[0]);
+				})
+				.catch(err => res.status(400).json('unable to get user'))
+			} else {
+				res.status(400).json('wrong password');
+			}
+		})
+	.catch(err => res.status(400).json('wrong credentials'));
 })
 
 app.post('/register', (req, res) => {
@@ -75,9 +85,9 @@ app.post('/register', (req, res) => {
 			})
 		})
 		.then(trx.commit)
-		.catch(trx.rollback)
+		.then(trx.rollback)
 	})
-	.catch(err => res.status(400).json('smth went wrong'))
+	.catch(err => res.status(400).json('smth went wrong'));
 })
 
 app.get('/profile/:id', (req, res) => {
