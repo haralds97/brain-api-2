ff90ae7967e99c2bf10432fc93984651670f7fb2
haraldsDev <Haralds.Dev@gmail.com>
2018-10-18 10:01:32 +0300

    one more time db Rec

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'postgres',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'smart5'
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+ 
 const database = {
 	users: [
 		{
@@ -42,15 +53,15 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { name, email, password } = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date()
-	});
-		res.json(database.users[database.users.length-1]);
+	db('users')
+		.returning('*')
+		.insert({
+			email: email,
+			name: name,
+			joined: new Date()
+		})
+	.then(user => res.json(user[0]))
+	.catch(err => res.status(400).json('smth went wrong'));
 })
 
 app.get('/profile/:id', (req,res) => {
