b407ad9639fe94a6ff4cf650557721eefc15254a
haraldsDev <Haralds.Dev@gmail.com>
2018-10-27 20:31:14 +0300

    fixing root path again

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors()); 
 
-app.get('/', (req, res) => { res.json('it is working!') })
+app.get('/', (req, res) => { res.json('it is working!') });
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) });
 
 
 app.listen(process.env.PORT || 3030, () => {
