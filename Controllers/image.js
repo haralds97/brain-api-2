8bd7f057c4eb47998fcd1c8e23a55123620e8109
haraldsDev <Haralds.Dev@gmail.com>
2018-11-22 01:19:27 +0200

    added missing endpoint, functions and syntax to backend

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,3 +1,8 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({ 
+ apiKey: '66c26976e675482eaa843e8fc6b634ca'
+});
 
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
@@ -10,6 +15,16 @@ const handleImage = (req, res, db) => {
 		.catch(err => res.status(400).json('failed to update entries'));
 }
 
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to do clarifai'));
+}
+
 module.exports = {
-	handleImage: handleImage
+	handleImage,
+	handleApiCall
 };
