92966370711d02993bee5ba748005c5d42e742b9
haraldsDev <Haralds.Dev@gmail.com>
2018-11-22 01:41:04 +0200

    small changes to backend to re-deploy on heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors()); 
 
-app.get('/', (req, res) => { res.json(database.users) })
+app.get('/', (req, res) => { res.json('it is working 3') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
@@ -31,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3030, () => {
-	console.log('app is running on port 3030');
+app.listen( process.env.PORT || 3030, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
